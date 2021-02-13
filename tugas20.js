44e04aba29df78bec6dd2d95b4fa322a63db2128
Rauf <zen.star12@yahoo.com>
2021-02-14 03:20:52 +0700

    Tugas 20 Javascript Niomic

---
+++ tugas20.js
@@ -0,0 +1,2 @@
+var x = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable";
+console.log(x.match("are"))
